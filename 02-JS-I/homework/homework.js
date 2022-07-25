59a43a5925093b1c52c9ab92caf4bdb68c10948a
Guido Strologo <guido.strologo@gmail.com>
2022-07-25 15:37:02 -0300

    Java 1 Resuelto

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -212,7 +212,7 @@ function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
   var area = alto * ancho;
-
+ 
   return area;
 }
 
