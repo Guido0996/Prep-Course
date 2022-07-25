ef9dc3043b82eb6f0edf85cb3a85183ebd975b1a
Guido Strologo <guido.strologo@gmail.com>
2022-07-25 15:35:28 -0300

    Java 3 Resuelto

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -143,10 +143,14 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí
-  var arr = [i];    
-  for (var i = 0; i < 10 ; i++){
-    if (i % 6 === 0) return arr
+  var seis = [];
+
+  for (var i = 0; i < 11 ; i++) {
+     
+    seis.push(6*i)
   }
+  return seis;
+
   
 }
 
